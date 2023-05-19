c30be39d2ecd364fd6c1ea71305c1d9df3ae0448
Red Monzon <redkmonzon30@gmail.com>
2023-05-19 11:54:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const trivia = (() => {  
+    let url = "";
+
+    const getUrl = () => {
+        return url
+    }
+
+    const setUrl = (value) => {
+        url = value;
+    };
+
+    return {getUrl, setUrl}
+})();
+
 const introForm = document.getElementById("intro-form");
 const questionsCount = document.getElementById("questions-count");
 const category = document.getElementById("category");
 const difficulty = document.getElementById("difficulty");
 const questionsType = document.getElementById("questions-type");
 
+introForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateInput();
+});
+
 const validateInput = () => {
+    const introSection = document.getElementById("intro-section");
     const error = document.getElementsByClassName("error")[0];
     const count = parseInt(questionsCount.value);
 
@@ -15,30 +35,34 @@ const validateInput = () => {
     } else {
         error.textContent = "";
         
-        const introSection = document.getElementById("intro-section");
         introSection.innerHTML = "";
+
+        startAsking();
     }
 }
 
-introForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    validateInput();
-});
-
 const generateUrl = () => {
-    let url = `https://opentdb.com/api.php?amount=${questionsCount}&category=${category.value}
+    let url = `https://opentdb.com/api.php?amount=${parseInt(questionsCount.value)}&category=${category.value}
                 &difficulty=${difficulty.value}&type=${questionsType.value}`;
 
     if (category.value === "" && difficulty.value === "" && questionsType.value === "") {
-        url = `https://opentdb.com/api.php?amount=${questionsCount}`;
+        url = `https://opentdb.com/api.php?amount=${parseInt(questionsCount.value)}`;
     } else if (category.value === "") {
-        url = `https://opentdb.com/api.php?amount=${questionsCount}&difficulty=${difficulty.value}&type=${questionsType.value}`;
+        url = `https://opentdb.com/api.php?amount=${parseInt(questionsCount.value)}&difficulty=${difficulty.value}&type=${questionsType.value}`;
     } else if (difficulty.value === "") {
-        url = `https://opentdb.com/api.php?amount=${questionsCount}&category=${category.value}&type=${questionsType.value}`;
+        url = `https://opentdb.com/api.php?amount=${parseInt(questionsCount.value)}&category=${category.value}&type=${questionsType.value}`;
     } else if (questionsType.value === "") {
-        url = `https://opentdb.com/api.php?amount=${questionsCount}&category=${category.value}&difficulty=${difficulty.value}`;
+        url = `https://opentdb.com/api.php?amount=${parseInt(questionsCount.value)}&category=${category.value}&difficulty=${difficulty.value}`;
     }
 
     return url;
 }
+
+const startAsking = () => {
+    trivia.setUrl(generateUrl());
+
+    const main = document.getElementsByTagName("main")[0];
+    main.style.display = "block";
+    
+}
+
